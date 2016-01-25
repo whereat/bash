d488a9387cca064c4974a2806d969b6217d97e4e
aguestuser <aguestuser@riseup.net>
2016-01-25 16:23:25 -0500

    Fix filepath problems

--- src/git-config/add-gpg-to-all.sh
+++ src/git-config/add-gpg-to-all.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# usage: KEY=<your_secret_key_short_fingerprint> $SCRIPTS/src/scripts/git-config/add-gpg-to-all.sh
+# usage:
+  # $ export PROJECT_ROOT="path/to/your/project/root"
+  # $ export SCRIPTS="path/to/whereat-bash"
+  # KEY=<your_secret_key_short_fingerprint> $SCRIPTS/src/scripts/git-config/add-gpg-to-all.sh
+
+cd ${PROJECT_ROOT}
 
 while read REPO
 do
     cd ${REPO} &&
-        ${SCRIPTS}/src/scripts/git-config/add-gpg-to-one.sh &&
+        ${SCRIPTS}/src/git-config/add-gpg-to-one.sh &&
         cd ..;
 done < ${SCRIPTS}/assets/repos.txt
 
