743cd3181dce0ffa5bff3447d7de0f6b381b3767
Alex Jablonski <ajablonski@thoughtworks.com>
2016-02-09 12:06:36 -0500

    AJ Allow Docker forwarding port to be configurable but default to 5000

--- src/docker/connect-to-location-server.sh
+++ src/docker/connect-to-location-server.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 set -x
 
-# usage: ${WHEREAT_SCRIPTS}/src/build/build-location-server.sh
+# usage: ${WHEREAT_SCRIPTS}/src/build/build-location-server.sh [PORT]
 
 cd "$WHEREAT_ROOT"
-REPO=whereat-location-server
+repo=whereat-location-server
+port=5000
 
-if [ ! -d "$REPO" ]; then
-    REPO="$REPO" ${WHEREAT_SCRIPTS}/src/remotes/clone-repo.sh
-    cd ${REPO}
-    REPO="$REPO" ${WHEREAT_SCRIPTS}/src/git-config/add-gpg-to-one.sh
-    cd $WHEREAT_ROOT
+if [ $# -eq 1 ]; then
+    port=$1
 fi
 
+if [ ! -d "$repo" ]; then
+    repo="$repo" ${WHEREAT_SCRIPTS}/src/remotes/clone-repo.sh
+    cd ${repo}
+    repo="$repo" ${WHEREAT_SCRIPTS}/src/git-config/add-gpg-to-one.sh
+    cd $WHEREAT_ROOT
+fi
 
 docker ps -a | grep --quiet whereat-location-server
 
 if [ $? -eq 0 ]; then
     docker start -i whereat-location-server
 else
-    docker run -it --name ${REPO} -p 15000:5000 -v ${WHEREAT_ROOT}/${REPO}:/${REPO} whereat/${REPO}:0.1 bash
+    docker run -it --name ${repo} -p $port:5000 -v ${WHEREAT_ROOT}/${repo}:/${repo} whereat/${repo}:0.1 bash
 fi
