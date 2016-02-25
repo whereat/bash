3afb19f4fff1a5e58d50de12d0006d5ca5ce365b
Brad Menchl <brmenchl@gmail.com>
2016-02-25 17:08:20 -0500

    [#M189] - BM/AG - Clone repos into new output directory. Bundle output files in output directory.

--- src/getNonCoreContributions.sh
+++ src/getNonCoreContributions.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-cd ${WHEREAT_ROOT}
+mkdir output
+rm -f output/*
 
 while read REPO
 do
-  cd $REPO &&
-  rm -f ${WHEREAT_SCRIPTS}/${REPO}-contributions.csv &&
-  git log --format='%H %an' |
+  rm -rf ./${REPO}
+  REPO=${REPO} ${WHEREAT_SCRIPTS}/src/remotes/clone-repo.sh &&
+  cd ${REPO} &&
+  git log --format='%H %an' --all |
   grep -v -E 'Austin Guest|aguestuser|The Fisher King|Jack Laxson|madelyn.freed|aepyornis|ziggy|j-ak|madelynfreed|rocket-mouse|imipolex-g|pellam|Pellam'|
   cut -d ' ' -f1 |
   xargs -n1 git log --date=iso --pretty=format:'"%h","%an","%ad","%s"%n' -1 |
-  cat >> ${WHEREAT_SCRIPTS}/${REPO}-contributions.csv &&
+  cat >> ../output/${REPO}-contributions.csv &&
   cd ..;
 done < ${WHEREAT_SCRIPTS}/assets/repos.txt
